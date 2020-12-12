a59906cd466006485095a87ebe78f66d1c0d97ad
DevoutPrayer <597707323@qq.com>
2020-12-12 15:04:09 +0800

    fix header iformation missing

--- rtmp.c
+++ rtmp.c
@@ -19,98 +19,6 @@ static int64_t start_time=0;
 static char *infile_name = "/tmp/output.h264";
 static int ceshi;
 
-// int main(int argc,char* argv[])
-// {
-//         
-//         char *outfile_name = "rtmp://192.168.2.213:1935/live/room1";
-//         init_rtmp_streamer();
-        
-//         if((ret = avformat_open_input(&ifmt_ctx,infile_name,0,0)) < 0)
-//         {
-//                 fprintf(stderr, "Could not open input file '%s' %d", infile_name,ret);
-//                 return -1;
-//         }
-
-//         if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
-// 		fprintf(stderr, "Failed to retrieve input stream information");
-// 		return -1;
-//         }
-//         av_dump_format(ifmt_ctx, 0, infile_name, 0);
-
-
-//         start_time=av_gettime();
-//         while(1)
-//         {
-//                 AVStream *in_stream, *out_stream;
-// 		//获取一个AVPacket（Get an AVPacket）
-// 		ret = av_read_frame(ifmt_ctx, &pkt);
-// 		if (ret < 0)
-// 			break;
-//                 printf("pts=%d,dts=%d,dur=%d\n",pkt.pts,pkt.dts,pkt.duration);
-// 		//FIX：No PTS (Example: Raw H.264)
-// 		//Simple Write PTS
-// 		if(pkt.pts==AV_NOPTS_VALUE){
-// 			//Write PTS
-// 			AVRational time_base1=ifmt_ctx->streams[videoindex]->time_base;
-// 			//Duration between 2 frames (us)
-// 			int64_t calc_duration=(double)AV_TIME_BASE/av_q2d(ifmt_ctx->streams[videoindex]->r_frame_rate);
-// 			//Parameters
-// 			pkt.pts=(double)(frame_index*calc_duration)/(double)(av_q2d(time_base1)*AV_TIME_BASE);
-// 			pkt.dts=pkt.pts;
-// 			pkt.duration=(double)calc_duration/(double)(av_q2d(time_base1)*AV_TIME_BASE);
-// 		}
-// 		//Important:Delay
-// 		if(pkt.stream_index==videoindex){
-// 			AVRational time_base=ifmt_ctx->streams[videoindex]->time_base;
-// 			AVRational time_base_q={1,AV_TIME_BASE};
-// 			int64_t pts_time = av_rescale_q(pkt.dts, time_base, time_base_q);
-// 			int64_t now_time = av_gettime() - start_time;
-//                         av_usleep(50000);
-// 			if (pts_time > now_time)
-//                                 av_usleep(50000);
-// 				//av_usleep(pts_time - now_time);
-
-// 		}
- 
-// 		in_stream  = ifmt_ctx->streams[pkt.stream_index];
-// 		out_stream = ofmt_ctx->streams[pkt.stream_index];
-// 		/* copy packet */
-// 		//转换PTS/DTS（Convert PTS/DTS）
-// 		pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (enum AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-// 		pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (enum AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-// 		pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
-// 		pkt.pos = -1;
-// 		//Print to Screen
-// 		if(pkt.stream_index==videoindex){
-// 			printf("Send %8d video frames to output URL\n",frame_index);
-// 			frame_index++;
-// 		}
-// 		//ret = av_write_frame(ofmt_ctx, &pkt);
-// 		ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
- 
-// 		if (ret < 0) {
-// 			printf( "Error muxing packet\n");
-// 			break;
-// 		}
-		
-// 		av_free_packet(&pkt);
-
-//         }
-//         av_write_trailer(ofmt_ctx);
-
-// end:
-//         avformat_close_input(&ifmt_ctx);
-//         if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
-// 		avio_close(ofmt_ctx->pb);
-// 	avformat_free_context(ofmt_ctx);
-// 	if (ret < 0 && ret != AVERROR_EOF) {
-// 		printf( "Error occurred.\n");
-// 		return -1;
-// 	}
-
-//         return 0;
-// }
-
 int write_frame(uint8_t*data,int size)
 {
         pkt.size = size;
@@ -129,12 +37,7 @@ int write_frame(uint8_t*data,int size)
         }
         return 0;
 }
-enum AVPixelFormat get_format(struct AVCodecContext *s, const enum AVPixelFormat * fmt)
-{
-        printf("77888\n");
-        fmt = &(s->pix_fmt);
-        return s->pix_fmt;
-}
+
 
 
 int init_rtmp_streamer(char* stream)
@@ -176,50 +79,7 @@ outfile_name=stream;
                 printf("Failed allocating output stream!\n");
                 goto end;
         }
-        AVCodecContext *codec_ctx;
-        codec_ctx = avcodec_alloc_context3(0);
-        
-        codec_ctx->codec_type = AVMEDIA_TYPE_VIDEO;
-        codec_ctx->bit_rate = 0x1c;
-        codec_ctx->width = 640;
-        codec_ctx->height = 480;
-        //codec_ctx->coded_width = 0x12345678;
 
-        codec_ctx->coded_height = ifmt_ctx->streams[0]->codec->coded_height;
-        //avcodec_set_dimensions(codec_ctx,640,480);
-        codec_ctx->gop_size = 85;
-        codec_ctx->pix_fmt = 35;
-        codec_ctx->max_b_frames = 480;
-        codec_ctx->get_format = get_format;
-        printf("---------size = %d---------\n",ifmt_ctx->streams[0]->codec->coded_width);
-        printf("---------size = %d---------\n",ifmt_ctx->streams[0]->codec->coded_height);
-        for(int i = 0; i<60;i++)
-        {
-                printf("\n%3d-%3d:",i*10,i*10+10);
-                for(int j=0;j<10;j++)
-                {
-                        printf("%.2x ",*((char *)(codec_ctx)+10*i+j));
-                }
-        }
-        //00 0b d8 99
-        // 
-        // codec_ctx->bit_rate = 0x1c;
-        // codec_ctx->codec_type = AVMEDIA_TYPE_VIDEO;
-        // codec_ctx->pix_fmt =  AV_PIX_FMT_YUV420P;
-        // codec_ctx->codec_id = AV_CODEC_ID_H264;
-        // codec_ctx->profile = 100;
-        // codec_ctx->level = -99;
-        // codec_ctx->debug = FF_DEBUG_BUGS;
-        // codec_ctx->colorspace = ifmt_ctx->streams[0]->codec->colorspace;
-        // codec_ctx->color_primaries = ifmt_ctx->streams[0]->codec->color_primaries;
-        // codec_ctx->color_trc = ifmt_ctx->streams[0]->codec->color_trc;
-        // codec_ctx->color_range = ifmt_ctx->streams[0]->codec->color_range;
-        // codec_ctx->slices = ifmt_ctx->streams[0]->codec->slices;
-        // codec_ctx->extradata = ifmt_ctx->streams[0]->codec->extradata;
-        // codec_ctx->extradata_size = ifmt_ctx->streams[0]->codec->extradata_size;
-        // codec_ctx->code
-        out_stream->codec = codec_ctx;
-        
         ret = avcodec_copy_context(out_stream->codec,ifmt_ctx->streams[0]->codec);
         if(ret < 0)
         {
